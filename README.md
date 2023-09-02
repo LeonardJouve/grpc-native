6853285b4401afc897b598b07c65806a03cfc7fa
Léonard Jouve <leonard.jouve@gmail.com>
2023-09-02 15:49:58 +0200

    clean

--- README.md
+++ README.md
@@ -1,31 +1,3 @@
 # grpc-native
 
 Automate type safe React Native bindings generation for grpc
-
-## Installation
-
-```sh
-npm install grpc-native
-```
-
-## Usage
-
-```js
-import { multiply } from 'grpc-native';
-
-// ...
-
-const result = await multiply(3, 7);
-```
-
-## Contributing
-
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
-
-## License
-
-MIT
-
----
-
-Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'grpc-native' doesn't seem to be linked. Make sure: \n\n` +
+  "The package 'grpc-native' doesn't seem to be linked. Make sure: \n\n" +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const GrpcNative = NativeModules.GrpcNative
-  ? NativeModules.GrpcNative
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+const GrpcNative =
+  NativeModules.GrpcNative ??
+  new Proxy(
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    }
+  );
 
 export function multiply(a: number, b: number): Promise<number> {
   return GrpcNative.multiply(a, b);
